195199e7530531d19995cded96debe69c633ed16
yuta3617 <yuta3617@gmail.com>
2020-02-18 14:41:29 +0900

    不要なファイル削除

---
+++ components/hello.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <h1>Hello World20000000</h1>
+  
+</template>
+
+<script>
+  import hello from '~/components/hello.vue'
+
+  export default {
+    components: {
+      hello
+    }
+  }
+</script>
+
+<style>
+
+</style>
